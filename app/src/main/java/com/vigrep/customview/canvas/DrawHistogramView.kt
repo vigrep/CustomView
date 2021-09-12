71a8a75a49468129d440b71c0ddc95382e0008e3
vigrep <838544960@qq.com>
2021-09-13 06:47:33 +0800

    完善直方图

--- app/src/main/java/com/vigrep/customview/canvas/DrawHistogramView.kt
+++ app/src/main/java/com/vigrep/customview/canvas/DrawHistogramView.kt
@@ -2,37 +2,53 @@ package com.vigrep.customview.canvas
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
 class DrawHistogramView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
+    private var axisMargin = 100f
+
     private val paint = Paint()
-    private var yStartX = 100f
-    private var yStartY = 0f
-    private var yEndX = 100f
-    private var yEndY = 0f
-    private var xStartX = 0f
-    private var xStartY = 0f
+    private val paintStrokeWidth = 10f
     private var xEndX = 0f
     private var xEndY = 0f
+    private var yEndX = 0f
+    private var yEndY = 0f
+
+    private var originX = axisMargin
+    private var originY = 0f
+
+    private var data: FloatArray = floatArrayOf(100f, 120f, 60f)
+    private val preWidth = 100f
+    private val preMargin = 100f
 
     init {
-        paint.strokeWidth = 10f
+        paint.strokeWidth = paintStrokeWidth
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        yEndY = measuredHeight - 60f
-        xStartX = 60f
-        xStartY = measuredHeight - 100f
-        xEndX = measuredWidth.toFloat() - 60f
-        xEndY = xStartY
+        originX = axisMargin
+        originY = measuredHeight - axisMargin
 
-        canvas?.drawLine(yStartX, yStartY, yEndX, yEndY, paint)
-        canvas?.drawLine(xStartX, xStartY, xEndX, xEndY, paint)
+        xEndX = measuredWidth.toFloat()
+        xEndY = originY
+
+        yEndX = originX
+        yEndY = 0f
+
+        canvas?.drawLine(originX, originY, xEndX, xEndY, paint)
+        canvas?.drawLine(originX, originY, yEndX, yEndY, paint)
+
+        paint.color = Color.GREEN
+
+        for ((index, value) in data.withIndex()) {
+            canvas?.drawRect(originX + (index+1)*preMargin + index*preWidth, value, originX + (index+1)*preMargin + index*preWidth + preWidth, originY - paintStrokeWidth/2, paint)
+        }
     }
 
 }
