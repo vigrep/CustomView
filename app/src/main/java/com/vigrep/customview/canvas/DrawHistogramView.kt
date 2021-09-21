964fd7f2243e9ad472c89a648df594ff96ec1fa1
vigrep <838544960@qq.com>
2021-09-21 23:22:11 +0800

    完善柱状图

--- app/src/main/java/com/vigrep/customview/canvas/DrawHistogramView.kt
+++ app/src/main/java/com/vigrep/customview/canvas/DrawHistogramView.kt
@@ -7,6 +7,8 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
+data class HistogramItem(var name: String, var value: Float, var color: Int)
+
 class DrawHistogramView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
     private var axisMargin = 100f
@@ -22,6 +24,7 @@ class DrawHistogramView(context: Context?, attrs: AttributeSet?) : View(context,
     private var originY = 0f
 
     private var data: FloatArray = floatArrayOf(320f, 300f, 200f, 90f, 100f, 120f, 320f, 300f, 200f, 90f, 100f, 120f)
+    private val items: MutableList<HistogramItem> = ArrayList()
     private var preWidth = 100f
     private var preMargin = 100f
 
@@ -30,6 +33,12 @@ class DrawHistogramView(context: Context?, attrs: AttributeSet?) : View(context,
 
     init {
         paint.strokeWidth = paintStrokeWidth
+        items.add(HistogramItem("华为", 225.6f, Color.GREEN))
+        items.add(HistogramItem("小米", 220.4f, Color.GRAY))
+        items.add(HistogramItem("OPPO", 218.2f, Color.BLUE))
+        items.add(HistogramItem("iPhone", 216.8f, Color.YELLOW))
+        items.add(HistogramItem("VIVO", 210f, Color.CYAN))
+        items.add(HistogramItem("魅族", 209f, Color.BLACK))
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -48,22 +57,27 @@ class DrawHistogramView(context: Context?, attrs: AttributeSet?) : View(context,
         preWidth = xAxisWidth / data.size.toFloat()
         preMargin = preWidth / 4.toFloat()
 
+        paint.color = Color.BLACK
         canvas?.drawLine(originX, originY, xEndX, xEndY, paint)
         canvas?.drawLine(originX, originY, yEndX, yEndY, paint)
 
-        paint.color = Color.GREEN
-
         histogramMax = originY - yEndY
         var maxValue = 0f
-        for (value in data) {
-            maxValue = value.coerceAtLeast(maxValue)
+        for (item in items) {
+            maxValue = item.value.coerceAtLeast(maxValue)
         }
 
         var ratio = if (maxValue > histogramMax) histogramMax/maxValue else 1.0f
 
-        for ((index, value) in data.withIndex()) {
-            canvas?.drawRect(originX + preMargin + index*preWidth, histogramMax - value * ratio, originX + preMargin + index*preWidth + preWidth/2.toFloat(), originY - paintStrokeWidth/2, paint)
+        for ((index, item) in items.withIndex()) {
+            paint.color = item.color
+            canvas?.drawRect(originX + preMargin + index*preWidth, histogramMax - item.value * ratio, originX + preMargin + index*preWidth + preWidth/2.toFloat(), originY - paintStrokeWidth/2, paint)
         }
+    }
+
+    fun addItem(item: HistogramItem) {
+        items.add(item)
+        invalidate()
     }
 
 }
